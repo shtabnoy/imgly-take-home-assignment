100549cc2251256c19e323378a07fd09ab749021
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2024-10-17 19:37:45 +0200

    chore: update jest + rtl setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,15 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  testRegex: '.test.(tsx?)$',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }],
+    '^.+\\.(ts|tsx|js|jsx)?$': [
+      'ts-jest',
+      { babelConfig: true, tsconfig: 'tsconfig.app.json' },
+    ],
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!@react-dnd|react-dnd|dnd-core|react-dnd-html5-backend|@minoru/react-dnd-treeview)',
+  ],
 };
